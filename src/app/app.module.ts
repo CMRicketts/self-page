ada69fe2cfe76f79a707ff7c48e1024e45f7eadb
Caleb Ricketts <calebr@zillowgroup.com>
2018-06-07 11:41:54 -0500

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,10 @@ import { TitlepageComponent } from './titlepage/titlepage.component';
 import { ResumepageComponent } from './resumepage/resumepage.component';
 import { ContactpageComponent } from './contactpage/contactpage.component';
 import { InfopageComponent } from './infopage/infopage.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ProjectpageComponent } from './projectpage/projectpage.component';
+import { ProjectinfoComponent } from './projectinfo/projectinfo.component';
+import { MeinfoComponent } from './meinfo/meinfo.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ProjectpageComponent } from './projectpage/projectpage.component';
     ResumepageComponent,
     ContactpageComponent,
     InfopageComponent,
-    ProjectpageComponent
+    ProjectpageComponent,
+    ProjectinfoComponent,
+    MeinfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meinfo/meinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meinfo',
+  templateUrl: './meinfo.component.html',
+  styleUrls: ['./meinfo.component.css']
+})
+export class MeinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
